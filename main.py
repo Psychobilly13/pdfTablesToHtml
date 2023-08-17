2586e5888dd003db50f3cbfead669055af3d086a
Psychobilly13 <psuchomort18@gmail.com>
2023-08-17 14:59:34 +0200

    feat: added errors logic

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import uuid
 import html
 import tempfile
+import ghostscript
 from img2table.document import PDF
 from img2table.ocr import TesseractOCR
 from dotenv import load_dotenv
@@ -15,42 +16,46 @@ load_dotenv()
 # logic
 async def recognize_pdf_with_text(file: str):
     # read data from saving file
-    tables = camelot.read_pdf(file, pages='all', line_scale=40)
-
-    # create html string from pdf data
-    html = ""
-    for table in tables:
-        html += table.df.to_html()
+    try:
+        tables = camelot.read_pdf(file, pages='all', line_scale=40)
+        # create html string from pdf data
+        html = ""
+        for table in tables:
+            html += table.df.to_html()
     
-    return html
+        return html
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"recognizing_with_text_failed:\n{e}")
 
 async def recognize_pdf_with_image(file: str):
-    tesseract_ocr = TesseractOCR(n_threads=8, lang="rus")
-    # confidence
-    min_recognizing_confidence = int(os.getenv("MIN_RECOGNIZING_CONFIDENCE"))
+    try:
+        tesseract_ocr = TesseractOCR(n_threads=8, lang="rus")
+        # confidence
+        min_recognizing_confidence = int(os.getenv("MIN_RECOGNIZING_CONFIDENCE"))
 
-    pdf = PDF(src=file)
-    extracted_tables = pdf.extract_tables(ocr=tesseract_ocr, implicit_rows=True, borderless_tables=True, min_confidence=min_recognizing_confidence)
+        pdf = PDF(src=file)
+        extracted_tables = pdf.extract_tables(ocr=tesseract_ocr, implicit_rows=True, borderless_tables=True, min_confidence=min_recognizing_confidence)
 
-    # create html string from images of pdf
-    html = ''
-    for page, tables in extracted_tables.items():
-        for idx, table in enumerate(tables):
-            # create page and merge this with other pages
-            html += table.html_repr(title=f"Page {page + 1} - Extracted table # {idx + 1}")
+        # create html string from images of pdf
+        html = ''
+        for page, tables in extracted_tables.items():
+            for idx, table in enumerate(tables):
+                # create page and merge this with other pages
+                html += table.html_repr(title=f"Page {page + 1} - Extracted table # {idx + 1}")
     
-    return html
+        return html
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"recognizing_with_image_failed:\n{e}")
+
 
 # API
 app = FastAPI()
 
-@app.post("/")
+@app.post("/convert")
 async def recognize_pdf(file: UploadFile = File(...)):
     try:
-        10 / 0;
         # create file path
         local_db = tempfile.gettempdir()
-        print(local_db)
         file_path = f"{local_db}/{file.filename}"
 
         # save file
@@ -67,8 +72,7 @@ async def recognize_pdf(file: UploadFile = File(...)):
             result = await recognize_pdf_with_image(file_path)
 
         return result
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="something wrong")
-    
+    except HTTPException as e:
+        raise HTTPException(status_code=e.status_code, detail=e.detail)
 
 
